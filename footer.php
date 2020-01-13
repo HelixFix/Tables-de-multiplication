6b6d04537858477f3d14f39bbb1469c987dcab56
HelixFix <jerome.rinner@gmail.com>
2020-01-13 09:59:08 +0100

    supression cahier background

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
 
-<footer>© Copyright 2019 Jérôme RINNER - Politique de confidentialité - Qui sommes-nous - Cookies</footer>
+<!--<footer>© Copyright 2019 Jérôme RINNER - Politique de confidentialité - Qui sommes-nous - Cookies</footer>-->
--- revision.php
+++ revision.php
@@ -9,7 +9,7 @@ include("header.php");
             include("aside.php");
             ?>
         </aside>
-        <section>créer des checkbox pour la selection des tables par choix multiples avec stream_bucket_make_writeable<br><br>
+        <section>créer des checkbox pour la selection des tables par choix multiples avec <br><br>
             <form action="revision.php" method="get">
 
                 <input type="checkbox" name="1">
@@ -22,7 +22,7 @@ include("header.php");
                 <label for="3">Table de 3</label>
 
                 <input type="checkbox" name="4">
-                <label for="4">Table de 5</label>
+                <label for="4">Table de 4</label>
 
                 <input type="checkbox" name="5">
                 <label for="5">Table de 5</label>
@@ -32,21 +32,96 @@ include("header.php");
                 <button type="submit">Envoyer</button>
             </form>
             <div id="math">
+                <div id="table1" class="table">
+                <?php
+                if ($_GET["1"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo  $n = $l . 'x' . 1 . '=' . $n = $l * 1 . '<br>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table2" class="table">
+                <?php
+                if ($_GET["2"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo $n = $l . 'x' . 2 . '=' . $n = $l * 2 . '<br>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table3" class="table">
+                <?php
+                if ($_GET["3"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 3 . '=' . $n = $l * 3 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table4" class="table">
+                <?php
+                if ($_GET["4"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 4 . '=' . $n = $l * 4 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table5" class="table">
+                <?php
+                if ($_GET["5"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 5 . '=' . $n = $l * 5 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table6" class="table">
                 <?php
                 if ($_GET["6"] === 'on') {
-                    echo '<img class="cahier" src="images/pageCahier.png" alt="">';
+                    
                     for ($l = 0; $l < 10; $l++) {
-                        
-                            echo '<div class="table">' . $n = $l . 'x' . 6 . '=' . $n = $l * 6 . '<br></div>';
-                        }
-                    }if ($_GET["5"] === 'on') {
-                        echo '<img class="cahier" src="images/pageCahier.png" alt="">';
-                        for ($l = 0; $l < 10; $l++) {
-                            
-                                echo '<div class="table">' . $n = $l . 'x' . 5 . '=' . $n = $l * 5 . '<br></div>';
-                            }
-                        }
+                        echo '<div class="table">' . $n = $l . 'x' . 6 . '=' . $n = $l * 6 . '<br></div>';
+                    }
+                }
                 ?>
+                </div>
+                <div id="table7" class="table">
+                <?php
+                if ($_GET["7"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 7 . '=' . $n = $l * 7 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table8" class="table">
+                <?php
+                if ($_GET["8"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 8 . '=' . $n = $l * 8 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
+                <div id="table9" class="table">
+                <?php
+                if ($_GET["9"] === 'on') {
+                    
+                    for ($l = 0; $l < 10; $l++) {
+                        echo '<div class="table">' . $n = $l . 'x' . 9 . '=' . $n = $l * 9 . '<br></div>';
+                    }
+                }
+                ?>
+                </div>
             </div>
 
 
