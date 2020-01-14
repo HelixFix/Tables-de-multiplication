ef853b16f169dab88ef8e51660e85dbd774468b5
HelixFix <jerome.rinner@gmail.com>
2020-01-14 09:53:19 +0100

    random tableau en brouillon

--- brouillon.php
+++ brouillon.php
@@ -1,63 +1,44 @@
+
+<form action="brouillon.php" method="get">
+    <input list="tableau" name="tableau">
+    <datalist id="tableau">
+        <option value="1">Table de 1</option>
+        <option value="Table de 2">
+        <option value="Table de 3">
+        <option value="Table de 4">
+        <option value="Table de 5">
+        <option value="Table de 6">
+        <option value="Table de 7">
+        <option value="Table de 8">
+        <option value="Table de 9">
+    </datalist>
+    <input type="submit">
+</form>
 <?php
 
 
-/*function multiplication()
-{
-    for ($i = 0; $i <= 9; $i++) {
-        echo $i . '<br>';
+$table = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+
+
+
+
+$randIndex = array_rand($table);
+
+// output the value for the random index
+echo '<p>' . 'le faire avec un random ? <p>';
+
+
+
+
+
+if (isset($_GET["tableau"]) == '1') {
+    $randIndex = array_rand($table);
+    $n = $randIndex * 1;
+    echo '<div><form action="brouillon.php" method="get">';
+    echo  $n = $randIndex . ' x ' . 1 . ' =  <input type="number" name="somme"> <input type="submit" value="Envoyer"><br><br></form>';
+    if ((isset($_GET["somme"]) == '$n')) {
+        echo '<span>true</span>';
     }
+    echo '</div>';
 }
-
-multiplication();*/
-
-
-
-// fonction multiplication
-/*function multi()
-{
-    $n = 10;
-    for ($l = 0; $l < 10; $l++) {
-        for ($c = 0; $c < 10; $c++) {
-            echo $n = $l * $c . '<br>';
-        }
-    }
-}*/
-
-
-/*foreach ($c=3) {
-    
-}*/
-
-
-$multiple = "3.14";
-$int = (int)$num;
-$float = (float)$num;
-
-
-$_GET["6"]
-
-
-{
-    
-    for ($l = 0; $l < 10; $l++) {
-        for ($c = 0; $c < 10; $c++) {
-            echo $n = $l . 'x' . $c . '=' . $n = $l * $c . '<br>';
-        }
-    }
-}
-
-function multi() {
-    $c = $item["table"];
-    
-    for ($l = 0; $l < 10; $l++) {
-        
-            echo $n = $l * $c . '<br>';
-}
-}
-
- multi();
-
-
- //$table = [1'x'1,1'x'2]
-
- 
+?>
